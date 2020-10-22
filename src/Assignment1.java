e203f1b29ffc73d651ceecf48d4eb3af5676cf39
GalSabas <galsabas@mail.tau.ac.il>
2020-10-22 12:52:40 +0300

    intial implementation

---
+++ src/Assignment1.java
@@ -0,0 +1,27 @@
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		int z = Integer.parseInt(args[2]);
+		if (x <= 0 || y <= 0 || z <= 0 ) {
+			System.out.println("Invalid input!");
+		}
+		else {
+			if ((x + y < z) || (x + z < y) || (y + z < x)) {
+				System.out.println("The input ("+x+", "+y+", "+z+") does not define a valid triangle!");
+			}
+			else {
+				int square_x = x * x;
+				int square_y = y * y;
+				int square_z = z * z;
+				if ((square_x + square_y == square_z) || (square_x + square_z == square_y) || (square_y + square_z == square_x)) {
+					System.out.println("The input ("+x+", "+y+", "+z+") defines a valid right triangle!");
+				}
+				else {
+					System.out.println("The input ("+x+", "+y+", "+z+") defines a valid triangle!");
+				}
+			}
+		}
+	}
+}
