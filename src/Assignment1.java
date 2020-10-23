c3a198cc13d8415046ebf93baa9ee2e3d0581269
GalSabas <galsabas@mail.tau.ac.il>
2020-10-23 18:18:04 +0300

    fix curly brackets

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,8 +1,6 @@
 
-public class Assignment1 
-{
-	public static void main(String[] args) 
-	{
+public class Assignment1 {
+	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]);
 		int y = Integer.parseInt(args[1]);
 		int z = Integer.parseInt(args[2]);
